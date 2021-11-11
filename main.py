87f3cd37a18f26138d65e2f4a454311c1413d1b7
pashkevich <pavsav.04@gmail.com>
2021-11-11 18:54:35 +0500

    inter

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.utils import get_random_id
 
 
 TOKEN = 'ef945ff25f70fd0434fc50668ce96b4083c28ab1f4893ca0da45bcbcfc38bdbfa5b7149153a3660f2a84f'
+VK_SESSION = vk_api.VkApi(token=TOKEN)
+VK = VK_SESSION.get_api()
+VK_LP = VkLongPoll(VK_SESSION)
+
+
+def send(id, msg):
+    VK.messages.send(user_id=id, message=msg, random_id=get_random_id())
 
 
 def main():
-    bot = vk_api.VkApi(token=TOKEN)
-    lp = VkLongPoll(bot)
-    for event in lp.listen():
+    for event in VK_LP.listen():
         if event.type == VkEventType.MESSAGE_NEW:
-            print(event)
+            if event.text != '' and event.from_user:
+                send(event.obj.from_id, 'Ну что, будем тренироваться?')
 
 
 if __name__ == '__main__':
