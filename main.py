c2023a3dddcba562988fc146405679c73a873cbe
pashkevich <pavsav.04@gmail.com>
2021-11-11 22:57:42 +0500

    update at keyboard

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
-from vk_api.utils import get_random_id
 
 
 TOKEN = 'ef945ff25f70fd0434fc50668ce96b4083c28ab1f4893ca0da45bcbcfc38bdbfa5b7149153a3660f2a84f'
@@ -11,14 +10,35 @@ VK_LP = VkLongPoll(VK_SESSION)
 
 
 def send(id, msg):
-    VK.messages.send(user_id=id, message=msg, random_id=get_random_id())
+    VK.messages.send(user_id=id, message=msg, random_id=1)
+
+
+def create_keyboard():
+    keyboard = VkKeyboard(one_time=False)
+    #False Если клавиатура должна оставаться откртой после нажатия на кнопку
+    #True если она должна закрваться
+
+    keyboard.add_button("Закрыть", color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button("Кнопка", color=VkKeyboardColor.POSITIVE)
+
+    keyboard.add_line()#Обозначает добавление новой строки
+    keyboard.add_button("Кнопка", color=VkKeyboardColor.NEGATIVE)
+
+    keyboard.add_line()
+    keyboard.add_button("Кнопка", color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button("Кнопка", color=VkKeyboardColor.PRIMARY)
+
+    return keyboard.get_keyboard()
+    #Возвращает клавиатуру
 
 
 def main():
+    training = False
     for event in VK_LP.listen():
         if event.type == VkEventType.MESSAGE_NEW:
-            if event.text != '' and event.from_user:
-                send(event.obj.from_id, 'Ну что, будем тренироваться?')
+            if event.from_user:
+                keyboard = create_keyboard()
+                send(event.user_id, 'Ну что, будем тренироваться?')
 
 
 if __name__ == '__main__':
