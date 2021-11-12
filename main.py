ab9918fa86d6156967cdff4f327f45a5d49c3220
pashkevich <pavsav.04@gmail.com>
2021-11-12 19:40:40 +0500

    added working keyboard and info

--- main.py
+++ main.py
@@ -1,44 +1,52 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from config import *
+from random import randint
 
 
-TOKEN = 'ef945ff25f70fd0434fc50668ce96b4083c28ab1f4893ca0da45bcbcfc38bdbfa5b7149153a3660f2a84f'
 VK_SESSION = vk_api.VkApi(token=TOKEN)
 VK = VK_SESSION.get_api()
 VK_LP = VkLongPoll(VK_SESSION)
 
 
-def send(id, msg):
-    VK.messages.send(user_id=id, message=msg, random_id=1)
+def send(id, msg, key=None):
+    if key:
+        VK.messages.send(user_id=id, message=msg, random_id=randint(0, 2 ** 64), keyboard=key)
+    else:
+        VK.messages.send(user_id=id, message=msg, random_id=randint(0, 2 ** 64))
 
 
-def create_keyboard():
-    keyboard = VkKeyboard(one_time=False)
-    #False Если клавиатура должна оставаться откртой после нажатия на кнопку
-    #True если она должна закрваться
-
-    keyboard.add_button("Закрыть", color=VkKeyboardColor.PRIMARY)
-    keyboard.add_button("Кнопка", color=VkKeyboardColor.POSITIVE)
-
-    keyboard.add_line()#Обозначает добавление новой строки
-    keyboard.add_button("Кнопка", color=VkKeyboardColor.NEGATIVE)
-
-    keyboard.add_line()
-    keyboard.add_button("Кнопка", color=VkKeyboardColor.PRIMARY)
-    keyboard.add_button("Кнопка", color=VkKeyboardColor.PRIMARY)
-
+def create_keyboard(training=False):
+    keyboard = VkKeyboard(one_time=True)
+    if training:
+        pass
+    else:
+        keyboard.add_button("Тренировка 💪🏻", color=VkKeyboardColor.PRIMARY)
+        keyboard.add_button("Инфо ⓘ", color=VkKeyboardColor.SECONDARY)
     return keyboard.get_keyboard()
-    #Возвращает клавиатуру
 
 
 def main():
     training = False
+    points = 0
+    tasks = 0
     for event in VK_LP.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            if event.from_user:
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            keyboard = create_keyboard()
+            message = event.text.lower().split()[0]
+            if message == 'начать':
+                send(event.user_id, 'Привет! Я - Ян Гус, давай учить нудную часть русского языка вместе со мной!')
+                send(event.user_id, 'Ну что, будем тренироваться?', keyboard)
+            if message == 'тренировка':
+                training = True
+            elif message == 'инфо':
+                send(event.user_id, INFO)
+                send(event.user_id, 'Ну что, будем тренироваться?', keyboard)
+            else:
+                send(event.user_id, 'Ну что, будем тренироваться?', keyboard)
+            if training:
                 keyboard = create_keyboard()
-                send(event.user_id, 'Ну что, будем тренироваться?')
 
 
 if __name__ == '__main__':
